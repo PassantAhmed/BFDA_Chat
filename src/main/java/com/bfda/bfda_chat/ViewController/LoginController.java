629b791f887f775b7bdec9f785352405a1441295
ahmed <ahmedsobhy.elgawesh@hotmail.com>
2018-02-08 11:05:04 +0200

    adding Login FXML Design , with Just Matching with variables in Controller of login

--- src/main/java/com/bfda/bfda_chat/ViewController/LoginController.java
+++ src/main/java/com/bfda/bfda_chat/ViewController/LoginController.java
@@ -5,18 +5,27 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField userNameFieldForLogIn;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField passwordFieldForLogIn;
+    
+    @FXML
+    private Button loginButton;
+    
+    @FXML
+    private Hyperlink signupButton;
+    
+    
+    
     
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
