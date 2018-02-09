04f6cc8b37ec5492bee0affa072c1e13b48ed9aa
Muhammed Fawzy <mohamedfawzy1993@gmail.com>
2018-02-09 17:56:22 +0200

    Client Interfaces

---
+++ Client/src/main/java/model/UserAccountImpl.java
@@ -0,0 +1,24 @@
+package model;
+
+import beans.User;
+import clientInterfaces.UserAccountInterface;
+
+import java.util.List;
+
+public class UserAccountImpl implements UserAccountInterface {
+
+    public UserAccountImpl(Object controller){
+
+    }
+    public void updateUserStatus(String userID, String status) {
+
+    }
+
+    public void updateUserMode(String userID, String mode) {
+
+    }
+
+    public void updateUserContactList(String userID, List<User> Contacts) {
+
+    }
+}
