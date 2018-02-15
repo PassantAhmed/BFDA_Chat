347c56b149909b019c3f1bb9e539a21c1cb779e5
David Emil <David Emil@David>
2018-02-15 19:04:31 +0200

    Registration Edit

--- Server/src/main/java/model/ClientServerRegisterImp.java
+++ Server/src/main/java/model/ClientServerRegisterImp.java
@@ -3,12 +3,17 @@ package model;
 import beans.User;
 import client.interfaces.ClientObj;
 import server.interfaces.ClientServerRegister;
+import model.database.DatabaseUserOperation;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClientServerRegisterImp extends UnicastRemoteObject implements ClientServerRegister, Serializable {
 
@@ -48,6 +53,27 @@ public class ClientServerRegisterImp extends UnicastRemoteObject implements Clie
 
     @Override
     public Boolean newUserRegisteration(User user) throws RemoteException {
+        boolean registered= false;
+        try {
+            
+            String username = user.getUsername();
+            DatabaseUserOperation DBO = new DatabaseUserOperation();  
+            String  email = user.getEmail();
+            
+            if(DBO.isEmailExist(email) == true && DBO.isUsernameExist(username)== true){
+                // email or username is exists
+                registered = false;
+            }else{
+                // new user then register
+                if(DBO.clientRegister(user)==true)
+                    registered = true;
+                
+            }
+            
+            return registered;
+        } catch (SQLException ex) {
+            Logger.getLogger(ClientServerRegisterImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
 }
