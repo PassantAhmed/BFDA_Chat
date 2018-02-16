fe7bf9f476e6c37ccc4490d564c2907db78a512e
Passant <Passant@Passant>
2018-02-16 20:44:18 +0200

    Server Pane

--- Server/src/main/java/view/controller/DashboardController.java
+++ Server/src/main/java/view/controller/DashboardController.java
@@ -19,39 +19,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Passant
  */
-public class DashboardController implements Initializable{
+public class DashboardController implements Initializable {
 
-    @FXML private Pane basePane;
-    @FXML private Button viewClientsBtn;
-    @FXML private Button viewStatisticsBtn;
-    @FXML private Button announcementsBtn;
-    @FXML private Button settingsBtn;
+    @FXML
+    private Pane basePane;
+    @FXML
+    private Button viewClientsBtn;
+    @FXML
+    private Button viewStatisticsBtn;
+    @FXML
+    private Button announcementsBtn;
+    @FXML
+    private Button settingsBtn;
     private List<Button> buttons = new ArrayList<>();
 
-    private Node viewClientNode ;
+    private Node viewClientNode;
     private Node viewStatNode;
     private Node viewAnnounce;
     private Node viewSettings;
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         buttonsArray();
         initNodes();
+        try {
+            Node newLoadedPane = FXMLLoader.load(getClass().getResource("/fxml/StatisticsScene.fxml"));
+            basePane.getChildren().clear();
+            basePane.getChildren().add(viewStatNode);
+            setButtonStyle(viewStatisticsBtn);
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
 
     public void viewClient(ActionEvent actionEvent) throws IOException {
         basePane.getChildren().clear();
         basePane.getChildren().add(viewClientNode);
         setButtonStyle(viewClientsBtn);
     }
-
 
     public void viewStatistics(ActionEvent actionEvent) throws IOException {
         Node newLoadedPane = FXMLLoader.load(getClass().getResource("/fxml/StatisticsScene.fxml"));
@@ -77,29 +89,27 @@ public class DashboardController implements Initializable{
 
     }
 
-    public void setButtonStyle(Button btn)
-    {
-        for(Button button : buttons)
-        {
+    public void setButtonStyle(Button btn) {
+        for (Button button : buttons) {
             String NOT_Clicked = "-fx-background-color:  #b9798b;";
             String clicked = "-fx-background-color:  #b0697d;";
 
-            if(button == btn)
+            if (button == btn) {
                 button.setStyle(clicked);
-            else
+            } else {
                 button.setStyle(NOT_Clicked);
+            }
         }
     }
-    private void buttonsArray()
-    {
+
+    private void buttonsArray() {
         buttons.add(viewClientsBtn);
         buttons.add(viewStatisticsBtn);
         buttons.add(announcementsBtn);
         buttons.add(settingsBtn);
     }
 
-    private void initNodes()
-    {
+    private void initNodes() {
         try {
             viewClientNode = FXMLLoader.load(getClass().getResource("/fxml/ClientsScene.fxml"));
             viewStatNode = FXMLLoader.load(getClass().getResource("/fxml/StatisticsScene.fxml"));
@@ -109,10 +119,6 @@ public class DashboardController implements Initializable{
             e.printStackTrace();
         }
 
-
     }
-
-
-
 
 }
