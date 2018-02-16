598623dee8f15fb2610d2160c366f1b659d982b4
Passant <Passant@Passant>
2018-02-16 12:49:39 +0200

    UI Update_2

--- Client/src/main/java/view/controller/ProfileController.java
+++ Client/src/main/java/view/controller/ProfileController.java
@@ -11,14 +11,20 @@ import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 /**
  *
  * @author Passant
  */
-public class ProfileController {
+public class ProfileController implements Initializable{
     @FXML private JFXTextField userFullName;
     @FXML private JFXTextField userName;
     @FXML private JFXTextField userEmail;
@@ -30,4 +36,28 @@ public class ProfileController {
     @FXML private Label warningMessage;
     @FXML private JFXButton cancelBtn;
     @FXML private JFXButton saveChangesBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userStatusInit();
+    }
+    
+    public void userStatusInit(){
+        ObservableList<String> options = 
+            FXCollections.observableArrayList(
+                "Available",
+                "Away",
+                "Busy"
+            );
+        userStatus.getItems().addAll(options);
+    }
+    
+    public void saveChanges(ActionEvent actionEvent)  {    
+        saveChangesBtn.setDisable(false);
+    }
+    
+    public void cancelChanges(ActionEvent actionEvent)  { 
+        cancelBtn.setDisable(false);
+        saveChangesBtn.setDisable(false);
+    }
 }
